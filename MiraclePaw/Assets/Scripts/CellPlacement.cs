a0f8a1d8dcc6e01a6ca25eb9816fc65a556adcda
Ralstonnn <zigthehedge2002@gmail.com>
2020-04-14 14:38:13 +0500

    first commit

---
+++ MiraclePaw/Assets/Scripts/CellPlacement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellPlacement : MonoBehaviour
+{
+    public GameObject cellPref;
+    public int cellsAmmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < cellsAmmount; i++)
+        {
+            Instantiate(cellPref);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void JustTryingToPlaceCells()
+    {
+        
+    }
+
+    //void size()
+    //{
+    //    cellPref.transform.localScale = new Vector3(.5f, .5f, .5f);
+    //    cellPref.gameObject.transform.position = new Vector3(Random.Range(-512,512), Random.Range(-384,384), 0);
+    //}
+}
---
+++ MiraclePaw/Assets/Scripts/CellSizeAndPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellSizeAndPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        size();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void size()
+    {
+        this.transform.localScale = new Vector3(.5f, .5f, .5f);
+        this.gameObject.transform.position = new Vector3(Random.Range(-6f , 6f), Random.Range(-4.5f,4.5f), 0);
+    }
+}

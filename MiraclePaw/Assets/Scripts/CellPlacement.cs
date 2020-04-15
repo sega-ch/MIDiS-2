985891e2984ab34831282ba78647dc36f9ff2959
Ralstonnn <zigthehedge2002@gmail.com>
2020-04-15 11:58:29 +0500

    attemp to ceate random spawning

--- MiraclePaw/Assets/Scripts/CellPlacement.cs
+++ MiraclePaw/Assets/Scripts/CellPlacement.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class CellPlacement : MonoBehaviour
 {
     public GameObject cellPref;
-    public int cellsAmmount;
+    public static int cellsAmmount;
+    public static int cellAmmoun;
+    Transform[] objects = new Transform[10];
+    public float distance;
 
     // Start is called before the first frame update
     void Start()
-    {
+    { 
+        objects[0] = GameObject.Find("Screenshot_2").GetComponent<Transform>();
+        objects[1] = GameObject.Find("Screenshot_2 (1)").GetComponent<Transform>();
+        objects[2] = GameObject.Find("Screenshot_2 (2)").GetComponent<Transform>();
+        objects[3] = GameObject.Find("Screenshot_2 (3)").GetComponent<Transform>();
+        objects[4] = GameObject.Find("Screenshot_2 (4)").GetComponent<Transform>();
+        objects[5] = GameObject.Find("Screenshot_2 (5)").GetComponent<Transform>();
+        objects[6] = GameObject.Find("Screenshot_2 (6)").GetComponent<Transform>();
+        objects[7] = GameObject.Find("Screenshot_2 (7)").GetComponent<Transform>();
+        objects[8] = GameObject.Find("Screenshot_2 (8)").GetComponent<Transform>();
+        objects[9] = GameObject.Find("Screenshot_2 (9)").GetComponent<Transform>();
         for (int i = 0; i < cellsAmmount; i++)
         {
             Instantiate(cellPref);
@@ -19,17 +32,21 @@ public class CellPlacement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 1; j < 10; j++)
+            {
+                distance = Vector3.Distance(objects[i].position, objects[j].position); 
+                Debug.Log("Расстояние = " + distance);
+                Debug.Log("I =" + i + ";" + "J =" + j);
+            }
+           
+        }
+        
     }
 
     void JustTryingToPlaceCells()
     {
-        
-    }
 
-    //void size()
-    //{
-    //    cellPref.transform.localScale = new Vector3(.5f, .5f, .5f);
-    //    cellPref.gameObject.transform.position = new Vector3(Random.Range(-512,512), Random.Range(-384,384), 0);
-    //}
+    }
 }
--- MiraclePaw/Assets/Scripts/CellSizeAndPosition.cs
+++ MiraclePaw/Assets/Scripts/CellSizeAndPosition.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CellSizeAndPosition : MonoBehaviour
 {
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +20,9 @@ public class CellSizeAndPosition : MonoBehaviour
 
     void size()
     {
+        
         this.transform.localScale = new Vector3(.5f, .5f, .5f);
         this.gameObject.transform.position = new Vector3(Random.Range(-6f , 6f), Random.Range(-4.5f,4.5f), 0);
+        
     }
 }

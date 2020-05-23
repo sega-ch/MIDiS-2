fc4aab1808143878073393af70aa44e43bb39e70
Yurasanai <58928425+Yurasanai@users.noreply.github.com>
2020-05-23 20:35:29 +0500

    S derevom

--- Paw/Assets/Scripts/Klad_Up.cs
+++ Paw/Assets/Scripts/Klad_Up.cs
@@ -9,16 +9,39 @@ public class Klad_Up : MonoBehaviour
     public GameObject rad2;
     public GameObject rad3;
     public GameObject rad4;
+    public static int podnatoKladov = 0;
     public static bool koshelokUp = false; //поднят ли кошелек
     private void OnTriggerEnter(Collider other)//зона на которой происходт потбор предмета
     {   if (koshelokUp == false)//если не подобран кошель
         {
-            if (other.gameObject.tag == "Klad" || other.gameObject.tag == "KladGold")//проверяем этот ли клад по тегу
+            if (other.gameObject.tag == "Klad")//проверяем этот ли клад по тегу
             {
                 if(other.gameObject.CompareTag("Klad")) AutoAllocator.currentPointsAmmountOnTheField--;
                 Debug.Log(AutoAllocator.currentPointsAmmountOnTheField);
                 Debug.Log("Есть пробитие!");
                 Destroy(other.gameObject);//уничтажаем клад
+                podnatoKladov++;
+                if (RedactorKlada.Do_stadii_1 <= podnatoKladov && RedactorKlada.Do_stadii_2 < podnatoKladov)
+                {
+                    byte vibor;
+                    vibor = gameObject.GetComponent<RedactorKlada>().Stadiya(RedactorKlada.Kosto4ka_1, RedactorKlada.ZolotayaKosto4ka_1, RedactorKlada.Koshelok_1, RedactorKlada.Amylet_1);
+                    
+                }
+                if (RedactorKlada.Do_stadii_1 <= podnatoKladov && RedactorKlada.Do_stadii_2 < podnatoKladov)
+                {
+                    gameObject.GetComponent<RedactorKlada>().Stadiya(RedactorKlada.Kosto4ka_1, RedactorKlada.ZolotayaKosto4ka_1, RedactorKlada.Koshelok_1, RedactorKlada.Amylet_1);
+
+                }
+                if (RedactorKlada.Do_stadii_1 <= podnatoKladov && RedactorKlada.Do_stadii_2 < podnatoKladov)
+                {
+                    gameObject.GetComponent<RedactorKlada>().Stadiya(RedactorKlada.Kosto4ka_1, RedactorKlada.ZolotayaKosto4ka_1, RedactorKlada.Koshelok_1, RedactorKlada.Amylet_1);
+                    
+                }
+                if (RedactorKlada.Do_stadii_1 <= podnatoKladov && RedactorKlada.Do_stadii_2 < podnatoKladov)
+                {
+                    gameObject.GetComponent<RedactorKlada>().Stadiya(RedactorKlada.Kosto4ka_1, RedactorKlada.ZolotayaKosto4ka_1, RedactorKlada.Koshelok_1, RedactorKlada.Amylet_1);
+
+                }
                 dog.gameObject.GetComponent<Joystic_touch>().enabled = false;//отключаем передвежение для анимации
                 Invoke("Timef", 2);//запускаем метод через две секунды (если примерно столько будет анимация)
             }

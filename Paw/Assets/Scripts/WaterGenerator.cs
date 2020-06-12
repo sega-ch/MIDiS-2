aa2cd52838d95a54453eebaa708848a3828e8cd9
Yurasanai <58928425+Yurasanai@users.noreply.github.com>
2020-06-13 02:57:17 +0500

    6 fps!!!

---
+++ Paw/Assets/Scripts/WaterGenerator.cs
@@ -0,0 +1,50 @@
+﻿
+using UnityEngine;
+
+public class WaterGenerator : MonoBehaviour
+{
+    public int depth = 20;
+    public int width = 256;
+    public int height = 256;
+    public float scale = 20f;
+    public float offsetX = 100f;
+    public float offsetY = 100f;
+
+    void Start()
+    {
+        offsetX = Random.Range(0, 9999f);
+        offsetY = Random.Range(0, 9999f);
+    }
+    void Update()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+        offsetX += Time.deltaTime * 5f;
+       // offsetY += Time.deltaTime * 5f;
+    }
+    TerrainData GenerateTerrain (TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(width, depth, height);
+        terrainData.SetHeights(0, 0, GenerateHeights());
+        return terrainData;
+    }
+    float[,] GenerateHeights()
+    {
+        float[,] heights = new float[width, height];
+        for (int x = 0; x<width; x++)
+        {
+            for (int y = 0; y<height; y++)
+            {
+                heights[x, y] = CaculateHeight(x, y);
+            }
+        }
+        return heights;
+    }
+    float CaculateHeight (int x, int y)
+    {
+        float xCoord = (float)x / width * scale + offsetX;
+        float yCoord = (float)y / height * scale + offsetY;
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}

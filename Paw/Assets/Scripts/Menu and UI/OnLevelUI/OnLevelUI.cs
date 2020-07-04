1bd22897c6fb1781a507966c0f7351d0e751e4df
Ralstonnn <zigthehedge2002@gmail.com>
2020-07-04 21:20:28 +0500

    OnLevelUI

---
+++ Paw/Assets/Scripts/Menu and UI/OnLevelUI/OnLevelUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnLevelUI : MonoBehaviour
+{
+    public GameObject PausePnl;
+    public GameObject OnLevelUIPnl;
+
+
+    public void OnPauseBtnClick()
+    {
+        PausePnl.SetActive(true);
+        OnLevelUIPnl.SetActive(false);
+    }
+
+    public void OnContinueBtnClick()
+    {
+        OnLevelUIPnl.SetActive(true);
+        PausePnl.SetActive(false);
+    }
+}

d30832ae6f5b0d3645752d7a8cca8e3a66aee209
Yurasanai <58928425+Yurasanai@users.noreply.github.com>
2020-07-12 21:25:29 +0500

    Button audio 0.1

---
+++ Paw/Assets/Scripts/ButtonAudio.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAudio : MonoBehaviour
+{
+    public AudioSource aSource;
+
+    void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+    }
+
+    public void StartPlaying()
+    {
+        aSource.Play();
+    }
+
+    public void StopPlaying()
+    {
+        Invoke("AwaykStop", 1);
+    }
+
+    public void AwaykStop()
+    {
+        aSource.Stop();
+    }
+}
